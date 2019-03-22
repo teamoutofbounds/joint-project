a613cdc0b574f21d968b9a12ee68ab2fb9573646
Joan Palau <joanp4l4u@gmail.com>
2019-03-22 19:37:03 +0100

    Added file directories needed and defined mock functions to implement, linked main app urls file to magatzem app urls

---
+++ magatzem/urls.py
@@ -0,0 +1,9 @@
+# els links de l'aplicació van aqui
+# els de frontend, heu d'utilitzar les funcions de views que tenen la paraula mock
+
+from django.urls import path
+
+
+urlpatterns = [
+
+]
--- magatzem/views.py
+++ magatzem/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def entrada_producte_mock(request):
+    context = []
+
+    return render(request, 'insertar_ruta al html_aqui', context)
+
+
+def llista_sales_mock(request):
+    context = []
+
+    return render(request, 'insertar_ruta al html_aqui', context)
+
+
+def sala_mock(request):
+    context = []
+
+    return render(request, 'insertar_ruta al html_aqui', context)
+
+
+def selccionar_productes_mock(request):
+    context = []
+
+    return render(request, 'insertar_ruta al html_aqui', context)
+
+
+def selccionar_sala_mock(request):
+    context = []
+
+    return render(request, 'insertar_ruta al html_aqui', context)
+
+
+def rebre_notificacio_mock(request):
+    context = []
+
+    return render(request, 'insertar_ruta al html_aqui', context)
