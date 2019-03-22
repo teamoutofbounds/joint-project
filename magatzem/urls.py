e245a758ba67cb81cd0d0716b9737bf8e14f02a6
Joan Palau <joanp4l4u@gmail.com>
2019-03-22 22:40:13 +0100

    Added example of how to use a mock

--- magatzem/urls.py
+++ magatzem/urls.py
@@ -2,8 +2,8 @@
 # els de frontend, heu d'utilitzar les funcions de views que tenen la paraula mock
 
 from django.urls import path
-
+from . import views
 
 urlpatterns = [
-
+    path('exemples/', views.exemple_mock, name='exemple')
 ]
--- magatzem/views.py
+++ magatzem/views.py
@@ -2,38 +2,44 @@ from django.shortcuts import render
 
 # Create your views here.
 
+def exemple_mock(request):
+    context = {
+        'variable': 'Hello World!!',
+    }
+
+    return render(request, 'magatzem/exemple.html', context)
 
 def entrada_producte_mock(request):
-    context = []
+    context = {}
 
-    return render(request, 'insertar_ruta al html_aqui', context)
+    return render(request, 'insertar_ruta al html que utilitza el mock', context)
 
 
 def llista_sales_mock(request):
-    context = []
+    context = {}
 
-    return render(request, 'insertar_ruta al html_aqui', context)
+    return render(request, 'insertar_ruta al html que utilitza el mock', context)
 
 
 def sala_mock(request):
-    context = []
+    context = {}
 
-    return render(request, 'insertar_ruta al html_aqui', context)
+    return render(request, 'insertar_ruta al html que utilitza el mock', context)
 
 
 def selccionar_productes_mock(request):
-    context = []
+    context = {}
 
-    return render(request, 'insertar_ruta al html_aqui', context)
+    return render(request, 'insertar_ruta al html que utilitza el mock', context)
 
 
 def selccionar_sala_mock(request):
-    context = []
+    context = {}
 
-    return render(request, 'insertar_ruta al html_aqui', context)
+    return render(request, 'insertar_ruta al html que utilitza el mock', context)
 
 
 def rebre_notificacio_mock(request):
-    context = []
+    context = {}
 
-    return render(request, 'insertar_ruta al html_aqui', context)
+    return render(request, 'insertar_ruta al html que utilitza el mock', context)
