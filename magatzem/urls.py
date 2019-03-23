b6cc462b02a68b048c88ad672a1aa02198da29fe
Raul Roca Juncà <rrj1@alumnes.udl.cat>
2019-03-23 17:10:55 +0100

    Afegit login.html i pàgina de login

--- magatzem/urls.py
+++ magatzem/urls.py
@@ -1,9 +1,10 @@
 # els links de l'aplicació van aqui
 # els de frontend, heu d'utilitzar les funcions de views que tenen la paraula mock
 
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('exemples/', views.exemple_mock, name='exemple')
+    path('exemples/', views.exemple_mock, name='exemple'),
+    path('', include('django.contrib.auth.urls')), # S'han de crear usuaris
 ]
