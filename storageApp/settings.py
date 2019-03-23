2b34738e82c84efb7cb794171e342ad187f15421
Toni Pacheco <toni.pafe@gmail.com>
2019-03-23 14:10:12 +0100

    Corretgit un bug al fitxer settings que impedia que es poguessin fer mirations.

--- storageApp/settings.py
+++ storageApp/settings.py
@@ -31,14 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'magatzem.apps.MagatzemConfig',
+    'oficina.apps.OficinaConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'magatzem.apps.MagatzemConfig',
-    'oficina.apps.OficinaConfig',
 ]
 
 MIDDLEWARE = [
