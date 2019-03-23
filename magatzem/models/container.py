993a2ec2447f4a83d7e5b5c1e695aa874b29ddf0
Toni Pacheco <toni.pafe@gmail.com>
2019-03-23 22:04:52 +0100

    Refactor de la clase Container. Afegit un índex per permetre una cerca ràpida pel service level agreement.

--- magatzem/models/container.py
+++ magatzem/models/container.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class Container(models.Model):
-    QUANTITY_MAX_VALUE = 999999999
+    QUANTITY_MAX_VALUE = 1000000000
     HUM_MIN_VALUE = 0
     HUM_MAX_VALUE = 100
     TEMP_MIN_VALUE = -999
@@ -18,37 +18,59 @@ class Container(models.Model):
     hum_min = models.IntegerField()
     hum_max = models.IntegerField()
     quantity = models.IntegerField()
+    SLA = models.IntegerField()
 
-    def __init__(self, product_id, producer_id, limit, quantity, temp_min, temp_max, hum_min, hum_max):
-        if not isinstance(product_id, str) or not re.match(r'^(\w ?)+$', product_id):
-            raise ValueError()
-        if not isinstance(producer_id, str) or not re.match(r'^(\d{11})$', producer_id):
-            raise ValueError()
-        if not isinstance(limit, str) or not re.match(r'^(\d{2}/\d{2}/\d{4})$', limit):
-            raise ValueError()
-        if not isinstance(quantity, int) or quantity < 1 or quantity > Container.QUANTITY_MAX_VALUE:
-            raise ValueError()
-        if not isinstance(temp_min, int) or temp_min < Container.TEMP_MIN_VALUE:
-            raise ValueError()
-        if not isinstance(temp_max, int) or temp_max > Container.TEMP_MAX_VALUE:
-            raise ValueError()
-        if not isinstance(hum_min, int) or hum_min < Container.HUM_MIN_VALUE:
-            raise ValueError()
-        if not isinstance(hum_max, int) or hum_max > Container.HUM_MAX_VALUE:
-            raise ValueError()
-        if temp_min > temp_max:
-            raise ValueError()
-        if hum_min > hum_max:
-            raise ValueError()
+    def __init__(self, product_id, producer_id, limit, quantity, temp_min, temp_max, hum_min, hum_max, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
-        self.product_id = product_id
-        self.producer_id = producer_id
-        self.limit = limit
-        self.quantity = quantity
-        self.temp_min = temp_min
-        self.temp_max = temp_max
-        self.hum_min = hum_min
-        self.hum_max = hum_max
+        if self._has_product_id_correct_format(product_id) and \
+           self._has_producer_id_correct_format(producer_id) and \
+           self._has_limit_correct_format(limit) and \
+           self._is_quantity_allowed(quantity) and \
+           self._are_temperaures_allowed(temp_min, temp_max) and \
+           self._are_humidity_allowed(hum_min, hum_max):
+
+            self.product_id = product_id
+            self.producer_id = producer_id
+            self.limit = limit
+            self.quantity = quantity
+            self.temp_min = temp_min
+            self.temp_max = temp_max
+            self.hum_min = hum_min
+            self.hum_max = hum_max
+            self._set_service_level_agreement(limit)
+        else:
+            raise ValueError()
 
     def __str__(self):
         return Container.STR_PATTERN.format(self.product_id, self.producer_id, self.quantity, self.limit)
+
+#   CHECKING PARAMETERS
+#########################################################################################################
+
+    def _has_product_id_correct_format(self, product_id):
+        return isinstance(product_id, str) and re.match(r'^(\w ?)+$', product_id)
+
+    def _has_producer_id_correct_format(self, producer_id):
+        return isinstance(producer_id, str) and re.match(r'^(\d{11})$', producer_id)
+
+    def _has_limit_correct_format(self, limit):
+        return isinstance(limit, str) and re.match(r'^(\d{2}/\d{2}/\d{4})$', limit)
+
+    def _is_quantity_allowed(self, quantity):
+        return isinstance(quantity, int) and 0 < quantity < Container.QUANTITY_MAX_VALUE
+
+    def _are_temperaures_allowed(self, temp_min, temp_max):
+        return isinstance(temp_min, int) and isinstance(temp_max, int) and  \
+               Container.TEMP_MIN_VALUE <= temp_min <= temp_max <= Container.TEMP_MAX_VALUE
+
+    def _are_humidity_allowed(self, hum_min, hum_max):
+        return isinstance(hum_min, int) and isinstance(hum_max, int) and \
+               Container.HUM_MIN_VALUE <= hum_min <= hum_max <= Container.HUM_MAX_VALUE
+
+##########################################################################################################
+#
+
+    def _set_service_level_agreement(self, limit):
+        data = limit.split('/')
+        self.SLA = int(data[2] + data[1] + data[0])
