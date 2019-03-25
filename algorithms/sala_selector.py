95223e5769736f6b754b5799b5985617d257f613
Raul Roca Juncà <rrj1@alumnes.udl.cat>
2019-03-25 18:02:55 +0100

    Implementada versió 2 algoritme de selecció de sala, amb "edge cases"

--- algorithms/sala_selector.py
+++ algorithms/sala_selector.py
@@ -5,41 +5,27 @@ from operator import itemgetter
 # de ser un altre dictionary amb: id, temp_min, temp_max, humidity_max, humidity_min sales ha de ser una llista de
 # sales on cada sala es un dictionary amb com a minim: stock_maxim, stock_restant, temperatura, humidity
 
-def salaSelector(container, sales):
-	container_list = []
-	product = container['product']
-	for sala in sales:
-		if sala['stock_maxim'] - sala['stock_actual'] >= container['quantitat']:
-			if product['temp_min'] <= sala['temperatura'] <= product['temp_max']:
-				if product['humidity_min'] <= sala['humidity'] <= product['humidity_max']:
-					sala['left_stock'] = sala['stock_maxim'] - sala[
-						'stock_actual']  # Ja que no se si ho tenim a la base de dades ho faig aixi
-					container_list.append(sala)
-
-	return sorted(container_list, key=itemgetter('left_stock'))
-# La de dalt es la primera versió que nomes fica els que compleixen les condicions i te les ordene.
-
-
-# La de baix t'ho distribueix i afegeix una clau nova al dictionary de sala que distribueix els contenidors
-# prioritzant l'aplenar sales
-
 def salaSelectorv2(container, sales):
 	container_list = []
 	product = container['product']
 	for sala in sales:
-		if product['temp_min'] <= sala['temperatura'] <= product['temp_max']:
-			if product['humidity_min'] <= sala['humidity'] <= product['humidity_max']:
-				sala['left_stock'] = sala['stock_maxim'] - sala['stock_actual']
-				sala['new_containers'] = 0
-				container_list.append(sala)
+		if is_room_suitable(product, sala):
+			sala['left_stock'] = sala['stock_maxim'] - sala['stock_actual']
+			sala['new_containers'] = 0
+			container_list.append(sala)
 
-	container_list = sorted(container_list, key=itemgetter('left_stock'))
+	container_list = sorted(container_list, key=itemgetter('left_stock'), reverse=True)  # S'ordena descentment
 	quantity = container['quantity']
 
-	for sala in container_list:  # Ho recorrerem per els que tenen menys espai disponible.
+	for sala in container_list:  # Comprovem si hi ha una sala amb exactament la mateixa capacitat disponible
+		if sala['left_stock'] == quantity:  # que contenidors volem col·locar
+			sala['new_containers'] = quantity
+			return container_list
+
+	for sala in container_list:
 		if quantity > 0:
 			left_stock = sala['left_stock']
-			if left_stock >= quantity:
+			if left_stock > quantity:
 				sala['left_stock'] -= quantity
 				sala['new_containers'] = quantity
 				quantity = 0
@@ -48,6 +34,14 @@ def salaSelectorv2(container, sales):
 				quantity -= left_stock
 				sala['left_stock'] = 0
 		else:
-			break
+			return container_list
 
-	return container_list
+	if quantity == 0:  # En el cas que en l'última sala pot haver distribuit tots els contenidors l'if no faria el return
+		return container_list
+
+	return []  # S'hi s'arriba aquí significa que no hi ha solució per les sales obertes disponibles
+
+
+def is_room_suitable(product, sala):
+	return product['temp_min'] <= sala['temperatura'] <= product['temp_max'] and \
+		product['humidity_min'] <= sala['humidity'] <= product['humidity_max']
