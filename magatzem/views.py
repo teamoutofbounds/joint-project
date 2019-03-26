32d9f37230fba61ac9a7358dc9e1b9e10ce788c5
JoanPalau <joanp4l4u@gmail.com>
2019-03-26 22:28:52 +0100

    Created mocks and Modified functions and urls

--- magatzem/views.py
+++ magatzem/views.py
@@ -4,6 +4,7 @@ from django.db.models import Q
 
 from magatzem.models.room import Room
 from magatzem.models.task import Task
+from magatzem.models.container import Container
 
 
 class RoomList(ListView):
@@ -25,6 +26,7 @@ class RoomDetail(DetailView):
         context = super().get_context_data(**kwargs)
         context['tasks'] = Task.objects.filter(~Q(task_status=4),
                                                Q(origin_room=context['room']) | Q(destination_room=context['room']))
+        context['containers'] = Container.objects.filter(room=context['room']).defer('room')
         context['title'] = context['room'].title
         return context
 
@@ -84,13 +86,134 @@ def entrada_producte_mock(request):
 
 
 def llista_sales_mock(request):
-    context = {}
+    context = {
+        'room_list': [
+            {
+                'name': 'Sala 1',
+                'temp_min': 0,
+                'temp_max': 5,
+                'hum_min': 15,
+                'hum_max': 35,
+                'quantity': 25,
+                'limit': 50,
+                'room_status': 1
+            },
+            {
+                'name': 'Sala 2',
+                'temp_min': 12,
+                'temp_max': 25,
+                'hum_min': 25,
+                'hum_max': 35,
+                'quantity': 50,
+                'limit': 50,
+                'room_status': 1
+            },
+            {
+                'name': 'Sala 3',
+                'temp_min': -5,
+                'temp_max': 50,
+                'hum_min': 15,
+                'hum_max': 35,
+                'quantity': 10,
+                'limit': 50,
+                'room_status': 1
+            },
+            {
+                'name': 'Sala 4',
+                'temp_min': 0,
+                'temp_max': 0,
+                'hum_min': 0,
+                'hum_max': 0,
+                'quantity': 0,
+                'limit': 50,
+                'room_status': 0
+            }
+        ]
+
+    }
 
     return render(request, 'insertar_ruta al html que utilitza el mock', context)
 
 
 def sala_mock(request):
-    context = {}
+    context = {
+        'room': {
+            'name': 'Sala 1',
+            'temp_min': 0,
+            'temp_max': 5,
+            'hum_min': 15,
+            'hum_max': 35,
+            'quantity': 25,
+            'limit': 50,
+            'room_status': 1
+        },
+        'tasks': {
+            'description': 'Traslladar',
+            'containers': [
+                {
+                    'productor_id': '20199110001',
+                    'producte_id': 'PERA CONFERENCE',
+                    'limit': '08/08/2019',
+                    'temp_min': 5,
+                    'temp_max': 15,
+                    'hum_min': 45,
+                    'hum_max': 70,
+                    'quantitat': 8
+                }
+            ],
+            'origin_room': {
+                'name': 'Sala 1',
+                'temp_min': 0,
+                'temp_max': 5,
+                'hum_min': 15,
+                'hum_max': 35,
+                'quantity': 25,
+                'limit': 50,
+                'room_status': 1
+            },
+            'destination_room': {
+                'name': 'Sala 3',
+                'temp_min': -5,
+                'temp_max': 50,
+                'hum_min': 15,
+                'hum_max': 35,
+                'quantity': 10,
+                'limit': 50,
+                'room_status': 1
+            },
+            'task_type': 1,
+            'task_status': 1,
+        },
+        'contenidors': [
+            {'productor_id': '20199110001',
+             'producte_id': 'PERA CONFERENCE',
+             'limit': '08/08/2019',
+             'temp_min': 5,
+             'temp_max': 15,
+             'hum_min': 45,
+             'hum_max': 70,
+             'quantitat': 8
+             },
+            {'productor_id': '20199110001',
+             'producte_id': 'TABLONES CAOBA 7x25',
+             'limit': '25/02/2022',
+             'temp_min': -5,
+             'temp_max': 50,
+             'hum_min': 0,
+             'hum_max': 40,
+             'quantitat': 16
+             },
+            {'productor_id': '20199110001',
+             'producte_id': 'TABLONES EBANO 7x25',
+             'limit': '25/02/2022',
+             'temp_min': -5,
+             'temp_max': 50,
+             'hum_min': 0,
+             'hum_max': 40,
+             'quantitat': 12}
+        ],
+        'title': 'Sala 1'
+    }
 
     return render(request, 'insertar_ruta al html que utilitza el mock', context)
 
