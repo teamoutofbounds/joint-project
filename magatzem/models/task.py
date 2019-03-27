f718b7eadc1e81cf10ee1c2fc0b0ebbc1007897d
JoanPalau <joanp4l4u@gmail.com>
2019-03-27 23:39:39 +0100

    Dev: Added functions to assure conditions in sqlite3 to Task

--- magatzem/models/task.py
+++ magatzem/models/task.py
@@ -1,6 +1,7 @@
 from django.db import models
 from .container import Container
 from .room import Room
+from django.contrib.auth.models import User
 from simple_history.models import HistoricalRecords
 from django.core.validators import MaxValueValidator
 
@@ -27,17 +28,20 @@ class Task(models.Model):
                                                  choices=TYPE_CHOICES, default=1)
     task_status = models.PositiveSmallIntegerField(validators=[MaxValueValidator(MAX_STATUS_CHOICES_VALUE)],
                                                    choices=STATUS_CHOICES, default=0)
-    origin_room = models.ForeignKey(Room, related_name='origin', on_delete=models.CASCADE)
-    destination_room = models.ForeignKey(Room, related_name='destination', on_delete=models.CASCADE)
+    origin_room = models.ForeignKey(Room, related_name='origin', on_delete=models.PROTECT)
+    destination_room = models.ForeignKey(Room, related_name='destination', on_delete=models.PROTECT)
     containers = models.ForeignKey(Container, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True)
 
     # Contains all the changes of the object
     history = HistoricalRecords()
 
-    def __init__(self, description, task_type, task_status, origin_room, destination_room, containers, *args, **kwargs):
+    def __init__(self, description, task_type, task_status, origin_room, destination_room, containers, user,
+                 history, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        if self._is_different_origin_destination(origin_room, destination_room):
+        if self._is_different_origin_destination(origin_room, destination_room) and self._is_allowed_choice(task_type) \
+                and self._is_allowed_status(task_status) and self.is_allowed_user(user):
 
             self.description = description
             self.task_type = task_type
@@ -53,3 +57,12 @@ class Task(models.Model):
 
     def _is_different_origin_destination(self, origin, destination):
         return origin is not destination
+
+    def _is_allowed_choice(self, choice):
+        return 0 <= choice < 3
+
+    def _is_allowed_status(self, status):
+        return status == 0
+
+    def is_allowed_user(self, user):
+        return user.groups.filter(id=user.id, name='operari').exists()
