887cfd68ff811779c34a184c61c35608028c798e
JoanPalau <joanp4l4u@gmail.com>
2019-03-28 14:14:38 +0100

    Dev: Added all validators and verbose names

--- magatzem/models/room.py
+++ magatzem/models/room.py
@@ -1,5 +1,6 @@
 from django.db import models
-from django.core.validators import MaxValueValidator
+from django.core.validators import MaxValueValidator, MinValueValidator
+from simple_history.models import HistoricalRecords
 
 
 class Room(models.Model):
@@ -10,14 +11,20 @@ class Room(models.Model):
     )
     STR_PATTERN = "Nom: {}\tEstat: {}\tMaxima capacitat: {}\tOcupat: {}"
 
-    name = models.CharField(max_length=16, default='Nova Sala')
-    temp_min = models.SmallIntegerField()
-    temp_max = models.SmallIntegerField()
-    hum_min = models.PositiveSmallIntegerField()
-    hum_max = models.PositiveSmallIntegerField(validators=[MaxValueValidator(100)])
-    quantity = models.PositiveIntegerField(default=0)
-    limit = models.PositiveIntegerField()
-    room_status = models.PositiveSmallIntegerField(choices=STATUS_CHOICES, default=0, validators=[MaxValueValidator(1)])
+    name = models.CharField(max_length=16, default='Nova Sala', verbose_name='Nom')
+    temp_min = models.SmallIntegerField(validators=[MinValueValidator(-100), MaxValueValidator(100)],
+                                        verbose_name='Temperatura mínima (ºC)')
+    temp_max = models.SmallIntegerField(validators=[MinValueValidator(-100), MaxValueValidator(100)],
+                                        verbose_name='Temperatura màxima (ºC)')
+    hum_min = models.PositiveSmallIntegerField(validators=[MaxValueValidator(100)], verbose_name='Humitat mínima (%)')
+    hum_max = models.PositiveSmallIntegerField(validators=[MaxValueValidator(100)], verbose_name='Humitat màxima (%)')
+    quantity = models.PositiveIntegerField(default=0, verbose_name='Espai ocupat')
+    limit = models.PositiveIntegerField(verbose_name='Capacitat màxima')
+    room_status = models.PositiveSmallIntegerField(choices=STATUS_CHOICES, default=0, validators=[MaxValueValidator(1)],
+                                                   verbose_name='Estat')
+
+    # Contains all the changes of the object
+    history = HistoricalRecords()
 
     class Meta:
         ordering = ['-room_status', 'name']
--- magatzem/models/task.py
+++ magatzem/models/task.py
@@ -9,9 +9,9 @@ from django.core.validators import MaxValueValidator
 class Task(models.Model):
     MAX_TYPE_CHOICES_VALUE = 2
     TYPE_CHOICES = (
-        (0, "Entrada"),
-        (1, "Intern"),
-        (2, "Sortida"),
+        (0, "Moviment d' Entrada"),
+        (1, "Moviment Intern"),
+        (2, "Moviment de Sortida"),
     )
     MAX_STATUS_CHOICES_VALUE = 4
     STATUS_CHOICES = (
@@ -23,46 +23,19 @@ class Task(models.Model):
     )
     STR_PATTERN = "Origin: {}\tDestination: {}\t Containers_info:\t {}"
 
-    description = models.CharField(max_length=32)
+    description = models.CharField(max_length=32, verbose_name='Descripció')
     task_type = models.PositiveSmallIntegerField(validators=[MaxValueValidator(MAX_TYPE_CHOICES_VALUE)],
-                                                 choices=TYPE_CHOICES, default=1)
+                                                 choices=TYPE_CHOICES, default=1, verbose_name='Tipus')
     task_status = models.PositiveSmallIntegerField(validators=[MaxValueValidator(MAX_STATUS_CHOICES_VALUE)],
-                                                   choices=STATUS_CHOICES, default=0)
-    origin_room = models.ForeignKey(Room, related_name='origin', on_delete=models.PROTECT)
-    destination_room = models.ForeignKey(Room, related_name='destination', on_delete=models.PROTECT)
-    containers = models.ForeignKey(Container, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+                                                   choices=STATUS_CHOICES, default=0, verbose_name='Estat')
+    origin_room = models.ForeignKey(Room, related_name='origin', on_delete=models.PROTECT, verbose_name="Sala d'origen")
+    destination_room = models.ForeignKey(Room, related_name='destination', on_delete=models.PROTECT,
+                                         verbose_name="Sala destí")
+    containers = models.ForeignKey(Container, on_delete=models.SET_NULL, null=True, verbose_name='Contenidor')
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, verbose_name='Persona assignada')
 
     # Contains all the changes of the object
     history = HistoricalRecords()
 
-    def __init__(self, description, task_type, task_status, origin_room, destination_room, containers, user,
-                 history, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        if self._is_different_origin_destination(origin_room, destination_room) and self._is_allowed_choice(task_type) \
-                and self._is_allowed_status(task_status) and self.is_allowed_user(user):
-
-            self.description = description
-            self.task_type = task_type
-            self.task_status = task_status
-            self.origin_room = origin_room
-            self.destination_room = destination_room
-            self.containers = containers
-        else:
-            raise ValueError()
-
     def __str__(self):
         return Task.STR_PATTERN.format(self.origin_room, self.destination_room, self.containers)
-
-    def _is_different_origin_destination(self, origin, destination):
-        return origin is not destination
-
-    def _is_allowed_choice(self, choice):
-        return 0 <= choice < 3
-
-    def _is_allowed_status(self, status):
-        return status == 0
-
-    def is_allowed_user(self, user):
-        return user.groups.filter(id=user.id, name='operari').exists()
