670caa663fb3067df557ee9141e3a46e34d26f62
Sactorum <parcheado2347@gmail.com>
2019-04-08 11:03:55 +0200

    Dev: Afegit comprovació d'usuari al servei notificacions

--- magatzem/views.py
+++ magatzem/views.py
@@ -53,9 +53,14 @@ class NotificationsListView(ListView):
     model = Task
     context_object_name = 'tasks'
     template_name = 'magatzem/notification.html'
+
+    def get_queryset(self):
+        queryset = super(NotificationsListView, self).get_queryset()
+        queryset = queryset.filter(user=self.request.user)
+        return queryset
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['tasks'] = Task.objects.filter(user=r)
         context['title'] = 'Notificacions'
         return context
 
