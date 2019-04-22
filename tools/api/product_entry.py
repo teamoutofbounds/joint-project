0d2abd49e67f1f35a49600be64527712b0d88429
Raul Roca Juncà <rrj1@alumnes.udl.cat>
2019-04-22 12:00:15 +0200

    Started api call function

---
+++ tools/api/product_entry.py
@@ -0,0 +1,19 @@
+import base64
+import json
+from pip._vendor import requests
+
+
+class EntryHandler:
+
+	def generate_entry(self):
+		"""
+		try:
+			response = requests.get(
+				"link que funcioni",
+				params={
+
+				},
+			)
+
+			dic = json.loads(response.content)
+		"""
