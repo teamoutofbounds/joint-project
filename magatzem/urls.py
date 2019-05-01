39b02443951e1efb54ef01c52308e656e13c60d7
JoanPalau <joanp4l4u@gmail.com>
2019-05-01 10:07:40 +0200

    dev: Added url to Task panel

--- magatzem/urls.py
+++ magatzem/urls.py
@@ -3,14 +3,20 @@
 
 from django.urls import path, include
 
-from magatzem.views import RoomList, RoomDetail, NotificationsListView
+from magatzem.views import RoomList, RoomDetail, NotificationsListView, TaskPanelOperaris
 from . import views
 
 urlpatterns = [
+    # urls gestor
     path('gestor/', views.home_gestor, name='gestor-home'),
     path('operaris/', views.home_operari, name='operaris-home'),
     path('gestor/sales/', RoomList.as_view(), name='list-room'),
     path('gestor/sales/<int:pk>/', RoomDetail.as_view(), name='detail-room'),
+    path('gestor/tasques_operaris', TaskPanelOperaris.as_view(), name='panel-operaris'),
+    # urls operari
+    path('operari/user/', NotificationsListView.as_view(), name='notificacio'),
+    # urls tecnics
+    # urls mocks
     # path('gestor/entrades/new/', views.exemple_mock, name='new-arrival'),
     # path('gestor/moviments/new/', views.exemple_mock, name='new-movement'),
     # path('gestor/sortides/new', views.exemple_mock, name='new-exit'),
@@ -18,7 +24,6 @@ urlpatterns = [
     path('llista_sales_mock/', views.llista_sales_mock, name='llista-sales-mock'),
     path('sala_mock/', views.sala_mock, name='sala_mock'),
     path('operari/pepe-lopez/', views.rebre_notificacio_mock, name='notificacio-mock'),
-    path('operari/user/', NotificationsListView.as_view(), name='notificacio'),
     path('sel_sala_mock/', views.seleccionar_sala_mock, name='sel-sala-mock'),
     path('sel_prod_mock/', views.seleccionar_productes_mock, name='sel-prod-mock'),
     path('tasques_mock/', views.panel_tasks_mock, name='tasques-mock'),
