558191dfe35f4c7182fb9b486367cf993dbec17e
Victor Ferrer <viicc6@MBP-de-Victor.home>
2019-05-01 16:12:05 +0200

    features modified

--- features/steps/authentication.py
+++ features/steps/authentication.py
@@ -1,5 +1,4 @@
 from behave import *
-# from magatzem.models import *
 
 use_step_matcher("parse")
 
@@ -7,3 +6,21 @@ use_step_matcher("parse")
 def step_impl(context, username, password):
     from django.contrib.auth.models import User
     User.objects.create_user(username=username, email='user@example.com', password=password)
+
+@given('I login as user "{username}" with password "{password}"')
+def step_impl(context, username, password):
+    context.browser.visit(context.get_url('/login'))
+    form = context.browser.find_by_tag('form').first
+    context.browser.fill('username', username)
+    context.browser.fill('password', password)
+    form.find_by_value('login').first.click()
+    assert context.browser.is_text_present('User: ' + username)
+
+@given('I\'m not logged in')
+def step_impl(context):
+    context.browser.visit(context.get_url('logout')+'?next=/magatzem/')
+    assert context.browser.is_text_present('login')
+
+@then("I'm redirected to the login form")
+def step_impl(context):
+    assert context.browser.url.startswith(context.get_url('login'))
