012ffa6b59d79e9c1e37fc5b49adbb5471440799
Toni Pacheco <toni.pafe@gmail.com>
2019-05-05 14:14:06 +0200

    Created Product model.

---
+++ magatzem/models/Product.py
@@ -0,0 +1,23 @@
+import re
+from django.db import models
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy
+
+
+def validate_name(value):
+    if not re.match(r'^(\w ?)+$', value):
+        raise ValidationError(
+            gettext_lazy('%(value) no es un nom valid.'),
+            params={'value': value}
+        )
+
+
+class Product(models.Model):
+    STR_PATTERN = "Producte={} | Productor={}"
+
+    product_id = models.CharField(validators=[validate_name], max_length=64)
+    producer_id = models.CharField(validators=[validate_name], max_length=64)
+
+    def __str__(self):
+        return Product.STR_PATTERN.format(
+            self.product_id, self.producer_id)
