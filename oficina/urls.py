fa7bf0c1750923819427e4e5fd46ed7c008c3b79
JoanPalau <joanp4l4u@gmail.com>
2019-05-20 22:04:16 +0200

    dev: placed the functions and urls in the correct app folder

--- oficina/urls.py
+++ oficina/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 
 
 urlpatterns = [
-
+    path('ceo/', HomeCEO.as_view(), name='ceo-home'),
+    path('ceo/users/list/', views.list_users, name='list_users'),
+    path('ceo/users/create/', views.create_user_as_ceo, name='create-user-ceo'),
+    path('ceo/users/delete/<int:pk>/confirm', views.delete_user_as_ceo, name='delete-user-ceo'),
 ]
--- oficina/views.py
+++ oficina/views.py
@@ -1,3 +1,104 @@
-from django.shortcuts import render
+from datetime import date
+
+from django.contrib.auth import authenticate
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User, Group
+from django.shortcuts import render, redirect
+from django.views.generic import ListView
+
+from magatzem.models import SLA, ContainerGroup, Room, TaskOperari
+from magatzem.views import is_allowed
+from users.forms import SignUpForm
 
 # Create your views here.
+
+
+class HomeCEO(ListView, LoginRequiredMixin, UserPassesTestMixin):
+    """ THIS IS A TEMPORARY IMPLEMENTATION:
+        Needed to check if all works properly in the front end,
+        until the real implementation could be done.
+    """
+    model = Room
+    context_object_name = 'rooms'
+    template_name = 'oficina/home-ceo.html'
+    # permission variable
+    roles = ('CEO',)
+
+    def test_func(self):
+        return is_allowed(self.request.user, self.roles)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Home CEO'
+        # get last tasks
+        context['last_tasks'] = self.get_last_tasks()
+        # get room capacity
+        context['capacities'] = {}
+        for item in context['object_list']:
+            context['capacities'][item.name] = item.quantity * 100 / item.limit
+
+        return context
+
+    def get_last_tasks(self):
+        tasks = TaskOperari.objects.order_by('-date').filter(date=date.today())
+        return tasks
+
+
+def getContainersExpiringSLA(date):
+    # parse date
+    elems = str(date).split('-')
+    result = elems[2] + '/' + elems[1] + '/' + elems[0]
+
+    # get all possible slas
+    slas = SLA.objects.filter(limit=result)
+    containers = []
+    for sla in slas:
+        queryset = ContainerGroup.objects.filter(sla=sla)
+        for container in queryset:
+            if container not in containers:
+                containers.append(container)
+    return containers
+
+
+def list_users(request):
+    all_users = User.objects.all()
+    return render(request, 'users/list_users-ceo.html', {'all_users': all_users})
+
+
+def create_user_as_ceo(request):
+    gestor = Group.objects.get_or_create(name='Gestor')[0]
+    operari = Group.objects.get_or_create(name='Operari')[0]
+    tecnic = Group.objects.get_or_create(name='Tecnic')[0]
+    ceo = Group.objects.get_or_create(name='Ceo')[0]
+    # add permisions to every group here
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            created = authenticate(username=username, password=raw_password)
+            user_type = form.cleaned_data.get('user_type')
+            if user_type.name == "Gestor":
+                created.groups.add(gestor)
+                created.save()
+            elif user_type.name == "Operari":
+                created.groups.add(operari)
+                created.save()
+            elif user_type.name == "Tecnic":
+                created.groups.add(tecnic)
+                created.save()
+            elif user_type.name == "Ceo":
+                created.groups.add(ceo)
+                created.save()
+            return redirect('list_users')
+    else:
+        form = SignUpForm()
+    return render(request, 'users/usercreate-ceo.html', {'form': form})
+
+
+def delete_user_as_ceo(request, pk):
+    u = User.objects.get(pk=pk)
+    u.delete()
+
+    return redirect('list_users')
