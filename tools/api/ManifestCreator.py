ea346bcda08ff9d83c83a64b280fc61c138c23e1
raultds <rrj1@alumnes.udl.cat>
2019-05-21 18:21:59 +0200

    dev: added sortida container

--- tools/api/ManifestCreator.py
+++ tools/api/ManifestCreator.py
@@ -1,12 +1,17 @@
 from magatzem.models import Product, SLA, ManifestEntrance, ManifestContainer, ManifestDeparture, Manifest, ContainerGroup, Room
-
+import copy
 
 class ApiManifestCreator(object):
 
-    def __init__(self, ref, producer_id):
-        Manifest.objects.get_or_create(ref=ref)
+    def __init__(self, ref, producer_id, toLocation):
+        if toLocation == None: # Cas entrada
+            ManifestEntrance.objects.get_or_create(ref=ref, origin=producer_id)
+        else:   # Cas sortida
+            ManifestDeparture.objects.get_or_create(ref=ref, destination=toLocation)
+            self.container_list=[]
         self.manifest = Manifest.objects.get(ref=ref)
         self.producer_id = producer_id
+        self.toLocation = toLocation
 
     def _create_entry_manifest(self, product):
         _product = Product.objects.get_or_create(product_id=product['name'],
@@ -33,88 +38,38 @@ class ApiManifestCreator(object):
                                                         sla=sla,
                                                         state=1)
 
+
     def _create_departure_manifest(self, product):
         _product = Product.objects.get(product_id=product['name'],
                                         producer_id=self.producer_id)
-        sla = SLA.objects.get(limit=product['sla'][:10],
-                                temp_min=product['tempMinDegree'],
-                                temp_max=product['tempMaxDegree'],
-                                hum_min=product['humidMin'],
-                                hum_max=product['humidMax'])
-        _product = Product.objects.get(product_id=product['name'])
-        sla = SLA.objects.get(limit=product['sla'][:10],
-                                        temp_min=product['tempMinDegree'],
-                                        temp_max=product['tempMaxDegree'],
-                                        hum_min=product['humidMin'],
-                                        hum_max=product['humidMax'])
-        manifest_container = ManifestContainer.objects.create(quantity=product['qty'],
-                                                                id_product=_product,
-                                                                id_SLA=sla,
-                                                                id_manifest=self.manifest)
+        sla = SLA.objects.get(limit=product['sla'][:10])
+                                #temp_min=product['tempMinDegree'],
+                                #temp_max=product['tempMaxDegree'],
+                                #hum_min=product['humidMin'],
+                                #hum_max=product['humidMax'])
 
+        container_group = ContainerGroup.objects.filter(id_product=_product,
+                                                        sla_id=sla).order_by('sla_id', 'quantity')
+        qty = product['qty']
+        for container in container_group:
+            if container.quantity < qty:
+                qty -= container.quantity
+                self.container_list.append(container)
+            elif container.quantity > qty:
+                container2 = copy.deepcopy(container)
+                container2.quantity = qty
+                container.quantity -= qty
+                container.save()
+                self.container_list.append(container2)
+            else:
+                qty -= container.quantity
+                self.container_list.append(container)
+                break
 
-
-
-
-
-
-
-
-
-
-
-
-
-    """
-    def __init__(self, product, transports):
-        self.limit = product['sla']
-        self.temp_min = product['tempMinDegree']
-        self.temp_max = product['tempMaxDegree']
-        self.hum_min = product['humidMin']
-        self.hum_max = product['humidMax']
-        self.quantity = product['qty']
-        self.name = product['name']
-        self.id_product = Product.objects.get(self.name)
-        self.id_SLA =
-        self.ref = transports['ref']
-        self.producer_id = transports['fromLocation']
-        self.id_manifest = ManifestEntrance.objects.get(self.ref)
-        self.destination = transports['toLocation']
-        self.date = transports['creationDate']
-
-
-    def _create_manifest_container_dependencies(self):
-        self._create_product()
-        self._create_sla()
-
-    def _create_product(self):
-        Product.objects.save(product_id=self.name,
-                             producer_id=self.producer_id)
-
-    def _create_sla(self):
-        SLA.objects.save(limit=self.limit,
-                         temp_min=self.temp_min,
-                         temp_max=self.temp_max,
-                         hum_min=self.hum_min,
-                         hum_max=self.hum_max)
-
-    def _create_manifest_container(self):
-        self._create_manifest_container_dependencies()
-        ManifestContainer.objects.save(quantity=self.quantity,
-                                       id_product=self.id_product,
-                                       id_SLA=self.id_SLA,
-                                       id_manifest=self.id_manifest)
-
-    def _create_manifest_departure(self):
-        ManifestDeparture.objects.save(ref=self.ref,
-                                       destination=self.destination,
-                                       date=self.creationDate)
-
-    def create_entry(self):
-        self._create_manifest_container()
-
-    def create_departure(self):
-        self._create_manifest_departure()
-        #TODO CREAR ALGORITME PER BUSCAR ELS MILLOR SLA I CREAR NOUS GRUP CONTAINERS I TAL
-
-        """
+        for container in self.container_list:
+            container.state=1
+            quantity = container.quantity - product['qty']
+            ManifestContainer.objects.create(quantity=container.quantity,
+                                                id_product=_product,
+                                                id_SLA=sla,
+                                                id_manifest=self.manifest)
