3a00a5b1a2ad7b97a4d817c5eb29270feecbbe1b
Sactorum <parcheado2347@gmail.com>
2019-05-21 22:00:33 +0200

    DEv: Tecnics full function and servide for manifests ceo

--- oficina/urls.py
+++ oficina/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
 from oficina import views
-from oficina.views import HomeCEO
+from oficina.views import HomeCEO, ManifestList,ManifestDetail
 
 urlpatterns = [
     path('ceo/', HomeCEO.as_view(), name='ceo-home'),
+    path('ceo/manifests', ManifestList.as_view(), name='manifests-ceo'),
+    path('ceo/manifests/<int:pk>', ManifestDetail.as_view(), name='manifests-detail-ceo'),
     path('ceo/users/list/', views.list_users, name='list_users'),
     path('ceo/users/create/', views.create_user_as_ceo, name='create-user-ceo'),
     path('ceo/users/delete/<int:pk>/confirm', views.delete_user_as_ceo, name='delete-user-ceo'),
